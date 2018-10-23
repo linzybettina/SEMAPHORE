91e809fd0a767ab0d6d0a9e9d1ad4a7228950487
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-23 20:01:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+
+class Semaphoredemo
+    {
+        static Semaphore semaphore = new Semaphore(5, 5);
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                new Thread(Semaphoredemo.DoSomething).Start(i);
+
+
+            }
+
+        }
+        static void DoSomething(object id)
+        {
+            Console.WriteLine(id +"Wants to access the Semaphore");
+            semaphore.WaitOne();
+            Console.WriteLine(id +"has succeeded to access the semaphore");
+            Thread.Sleep(3000);
+            Console.WriteLine(id +"is about to leave the semaphore");
+            semaphore.Release();
+            Console.ReadLine();
+
+        }
+
+
+    }
+
